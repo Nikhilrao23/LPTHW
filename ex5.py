0b9348d4e5fc61d50680a97a0423065adbacd0e6
Nikhilrao23 <raonikhil23@gmail.com>
2018-06-14 19:58:06 -0400

    Adding 5th Ex

---
+++ ex5.py
@@ -0,0 +1,34 @@
+name = 'Nikhil Rao'
+age = 26
+height = 6.2
+weight = 220
+eyes = "black"
+teeth = "white"
+hair = "Black"
+fingers = 2
+
+
+print "Let's talk about %s" %(name)
+
+print "He's %d inches tall" %(height)
+
+print "He's %d pounds heavy." %(weight)
+
+print "Actually that's not too heavy"
+
+print "He's got %s eyes and %s hair" %(eyes, hair)
+
+print "His teeth are usually %s depending on the coffee" %(teeth)
+
+print "If i add %d, %d and %d I get %d" %(age, height, weight, age + height + weight)
+
+print "His fingers are %r" %(fingers)
+
+# %r indicates print whatever is there
+
+
+
+
+
+
+
