3fdff2f69396f2c4e55582f532299d357c156ec5
Nikhilrao23 <raonikhil23@gmail.com>
2018-06-14 20:55:06 -0400

    Adding 9th Ex

---
+++ ex9.py
@@ -0,0 +1,17 @@
+days = "Mon Tues Wed Thrus Fri Sat Sun"
+
+months = "\nJan\n Feb\n Mar\n Apr\n May\n Jun\n Jul\n Aug\n Sep\n Oct\n Nov\n Dec\n"
+
+print "Here are the days: ", days
+
+print "Here are the Months: " ,months
+
+print """
+        There is something going on here.
+        With the three double quotes
+        We'll be able to type as much as we like
+        Even 4 lines if we want, or 5, or 6
+"""
+
+#Using %r with months doesnt work well because they print out raw values and this doesnt suit them up well
+
