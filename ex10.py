4e91087a7e21eafdc39d4b1fa15ca4b27df68809
Nikhilrao23 <raonikhil23@gmail.com>
2018-07-09 16:15:57 -0400

    Adding Few more Excercises to the list

---
+++ ex10.py
@@ -0,0 +1,25 @@
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """"
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print "Practice pannalamaaa"
+print "\T is for Horizontal line to be added onto the text"
+
+name_is = "This is something, but give me some \t* space"
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+print name_is
+
+
+while True:
+    for i in ["/", "-", "|", "\\", "|"]:
+        print "%s\r" %i,
