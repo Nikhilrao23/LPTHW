fb26adaae7b9184d76955b506ae19af58d1567a8
Nikhilrao23 <raonikhil23@gmail.com>
2018-07-11 11:44:52 -0400

    Ex19 Updated

--- ex19.py
+++ ex19.py
@@ -1,22 +1,20 @@
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print "You have %d cheeses!" % cheese_count
-    print "You have %d boxes!" % boxes_of_crackers
-    print "Men That's enough for the party"
-    print "Get a Blanket\n."
+    print "You have %d boxes" % boxes_of_crackers
+    print "Mans that enough for a party"
+    print "Get a Blanket. \n"
 
+print "We can give the function numbers directly"
+cheese_and_crackers(25, 10)
 
-print "We can give them direclty"
-cheese_and_crackers(23, 50)
-
-print "Or We can give variables"
-amount_of_cheese = 10
+print "We can assign them to a variable"
+amount_of_cheese = 25
 amount_of_boxes = 15
-
 cheese_and_crackers(amount_of_cheese, amount_of_boxes)
 
-print "We can do the math inside of this"
-cheese_and_crackers(10+5, 20-10)
+print "We can do math inside the arguement"
+cheese_and_crackers(10+5, 20 -6)
 
-print "We can combine both of them together"
-cheese_and_crackers(amount_of_cheese+100, amount_of_boxes+150)
+print "We can assign then and Calculate"
+cheese_and_crackers(amount_of_cheese+13, amount_of_boxes + 30)
 
